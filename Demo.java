0af271fcf807502630180094294e5650769783a9
Shreyas Moolya <65644820+shreyasmoolya007@users.noreply.github.com>
2022-08-24 09:38:57 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,110 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Demo {
+    public static void main(String[] args){
+        /*System.out.println("Shinichi");
+        int a=10;
+        float b=a;
+        double d=b;
+        System.out.println(d);
+
+        int a=10;
+        int b=5;
+
+        int result=a+b;
+        System.out.println(result);
+        int c=b-a;
+        System.out.println(c);
+        int d=a*b;
+        System.out.println(d);
+        float e=b/a;
+        System.out.println(e);
+
+        int a=3;
+        int b=20;
+        int c=30;
+
+
+       // int result=(a>b)?a:b;
+      // System.out.println(result);
+
+        String result=(a%2==0)?"even":"odd";
+        System.out.println(result);
+
+        int a=20;
+
+        System.out.println(++a);
+        System.out.println(a + "java");
+        System.out.println(++a + a + "james");
+
+        int a=23;
+
+        System.out.println(a++);
+        System.out.println(++a + a++ - a);
+        System.out.println("alvas" + a - ++a);
+
+        int a=20;
+        int b=40;
+
+        boolean result=a++>b && b++<a;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(result);
+
+        int a=20;
+        int b=40;
+
+        boolean result=++a<b||b++<a;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(result);
+
+        int a=15;
+        int b=25;
+
+        System.out.println(a++);
+        System.out.println(--b);
+        System.out.println(++a);
+        System.out.println(b--);
+        System.out.println(a++ + 45 + ++a);
+        System.out.println(--b + "VTU");
+
+        int age=20;
+        if(age>40)
+        {
+            System.out.println("Bye laila");
+        }
+        System.out.println(age++);
+        System.out.println(age++ + age);
+
+        int age=20;
+        if(age%2==0)
+        {
+            System.out.println("Even");
+
+        int a=10;
+        int b=20;
+
+        if(a<b)
+            System.out.println("b is larger");
+        else
+            System.out.println("a is larger");
+
+        Scanner s =new Scanner(System.in);
+        char ch=s.next().charAt(0);
+        if(ch)
+        {
+            System.out.println("Alphabet");*/
+
+        int a=15;
+
+        if(a%3==0 && a%5==0)
+            System.out.println("Divisible");
+        else
+            System.out.println("Not Divisible");
+
+    }
+}
