19782cc9788ff1c7ccb023f23d9d303d89f33292
Shreyas Moolya <65644820+shreyasmoolya007@users.noreply.github.com>
2022-08-27 08:48:31 +0530

    Add files via upload

---
+++ Loading.java
@@ -0,0 +1,27 @@
+public class Loading {
+    /*static String str;
+    public static void main(String[] args) {
+        System.out.println(str);*/
+
+    static int a;
+    static int b;
+
+    public static void main(String[] args) {
+        System.out.println(a);
+        System.out.println(b);
+        m1();
+        m2();
+        System.out.println(a);
+        System.out.println(b);
+    }
+
+    public static void m1(){
+        a=20;
+    }
+
+    public static void m2(){
+        b=40;
+    }
+
+
+}
