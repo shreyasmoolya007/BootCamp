246992f664921ab394bf1a7cc1ee5fc51473e184
Shreyas Moolya <65644820+shreyasmoolya007@users.noreply.github.com>
2022-09-03 09:26:30 +0530

    Add files via upload

---
+++ Strings/S2.java
@@ -0,0 +1,9 @@
+public class S2 {
+    public static void main(String[] args) {
+        String s1="Raja";
+        String s2=new String("Raja");
+
+        System.out.println(s1==s2);
+        System.out.println(s1.equals(s2));
+    }
+}
